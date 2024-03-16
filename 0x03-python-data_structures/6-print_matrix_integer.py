5563d58e86e3bec6a060689ec16ed73d0f2968a5
mennassaber <mennasaber202620@gmail.com>
2024-03-16 09:49:37 -0700

    python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,13 +5,4 @@ def print_matrix_integer(matrix=[[]]):
             print("{:d}".format(row[i]), end="")
             if i != len(row) - 1:
                 print(" ", end="")
-        print()
-if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-    print_matrix_integer(matrix)
-    print("--")
-    print_matrix_integer()
+        print(i)
