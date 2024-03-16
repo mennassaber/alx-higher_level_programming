a7f573606f950d64835e8a6fe465037221963ae5
mennassaber <mennasaber202620@gmail.com>
2024-03-16 10:04:58 -0700

    pyhon

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    """
-    Print a matrix of integers.
-
-    Args:
-        matrix (list of lists): The matrix to be printed.
-
-    Returns:
-        None
-    """
     for row in matrix:
         for i in range(len(row)):
             if i != 0:
                 print(" ", end="")
             print("{:d}".format(row[i]), end="")
         print()
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
-
