38d0876052de60acb1c93245e890685832c86546
mennassaber <mennasaber202620@gmail.com>
2024-03-16 09:04:19 -0700

    python

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,10 +5,3 @@ def replace_in_list(my_list, idx, element):
     else:
         my_list[idx] = element
         return my_list
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    new_element = 9
-    new_list = replace_in_list(my_list, idx, new_element)
-    print(new_list)
-    print(my_list)
