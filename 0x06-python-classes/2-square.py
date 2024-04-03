4f834c6375e7d29355adadbd73af200ed5b528da
mennassaber <mennasaber202620@gmail.com>
2024-04-02 18:51:53 -0700

    pyhon

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size=0):
-        self.__size = size
-    @property
+    def __init__(self,size=0):
+        self.size=size
     def size(self):
         return self.__size
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+    def size(self,value):
+        if not isinstance(value,int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value<0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size=value
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size=0):
-        self.__size = size
-    @property
+    def __init__(self,size=0):
+        self.size=size
     def size(self):
         return self.__size
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+    def size(self,value):
+        if not isinstance(value,int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value<0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size=value
     def area(self):
-        return self.__size ** 2
+        return self.__size**2
     def my_print(self):
-        if self.__size == 0:
+        if self.__size==0:
             print()
-            return
-        for _ in range(self.__size):
-            print("#" * self.__size)
+        else:
+            for _ in range(self.__size):
+                print("#"*self.__size)
