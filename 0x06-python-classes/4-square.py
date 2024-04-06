1d8b9972b1773b351497faa9aadbf628559d5b19
mennassaber <mennasaber202620@gmail.com>
2024-04-06 09:33:05 -0700

    .py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 class Square:
     def __init__(self, size=0):
         self.__size = size
@@ -6,11 +7,11 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
+    
     @property
     def size(self):
         return self.__size
-
+    
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
